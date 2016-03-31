d0ceabc4630920995194e49a565b75c67a534af2
Jeana Mobley <jeana.m.mobley@gmail.com>
2016-03-30 22:14:14 -0400

    Added files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -180,4 +180,22 @@ $(document).ready(function() {
 });
 
 
+$(document).ready(function() {
+    var pull = $('#pull');
+        menu = $('nav ul');
+        menuHeight = menu.height();
+ 
+    $("#nav-button").on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+
+    $(window).resize(function(){
+    var w = $(window).width();
+    if(w > 320 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+    }
+    }); 
+});
+
 
